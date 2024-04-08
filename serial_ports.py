b2f114d8e13662de916fdfa6359fc17b024ad9ae
Ole Kuhlmann <ole.kuhlmann@rwth-aachen.de>
2024-04-08 11:05:50 +0200

    initial commit; added serial_ports

---
+++ serial_ports.py
@@ -0,0 +1,10 @@
+"""
+    These are the serialport allocations for the 4 boards controlling the TECs when connected via USB.
+    TODO: test whether this changes over time / on different machines
+"""
+
+PORT_TOP_1 = "COM5"
+PORT_TOP_2 = "COM6"
+
+PORT_BOTTOM_1 = "COM3"
+PORT_BOTTOM_1 = "COM4"
