a8e64db07a5a3a9a0bb8dc3106b2d877ad7fe840
Ole Kuhlmann <ole.kuhlmann@rwth-aachen.de>
2024-04-12 10:38:52 +0200

    refactored callbacks using data store in own file

--- ui/callbacks.py
+++ ui/callbacks.py
@@ -15,11 +15,11 @@ from ui.components.graphs import (
     update_graph_object_temperature,
     update_graph_max_voltage,
 )
+from ui.data_store import get_data_from_store, update_store
 
 _tec_interface: TECInterface = None
 
-# timestamp of the last data pulled
-_last_data_timestamp = None
+
 
 
 def tec_interface():
@@ -29,55 +29,7 @@ def tec_interface():
     return _tec_interface
 
 
-def df_to_base64(df):
-    """
-    Converts a dataframe to base64 for storage
-    """
-    buffer = io.BytesIO()
-    df.to_parquet(buffer, index=True) # includes index
-    encoded = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    return encoded
 
-def base64_to_df(encoded):
-    """
-    Converts a base64 encoded dataframe back to a dataframe
-    """
-    data = base64.b64decode(encoded)
-    buffer = io.BytesIO(data)
-    df = pd.read_parquet(buffer)
-    return df
-
-
-def get_data_from_store(store_data, most_recent=True):
-    """Parses the data store and returns a dataframe with the data
-
-    Args:
-        store_data (string): base64 data from the data store
-        most_recent (bool, optional): When set to true, only the most recent measurement is returned. When set to false, all recorded measurements are returned. Defaults to True.
-    """
-    if store_data is None:
-        return None
-
-    # convert json to df
-    df = base64_to_df(store_data)
-    
-    if df.empty:
-        return df
-
-    if most_recent:
-        # get the most recent timestamp from the last row and return all rows with that one
-        last_index = len(df) - 1
-        most_recent_timestamp = df.iloc[last_index]["timestamp"]
-
-        # Iterate backwards until a different timestamp is found
-        for i in range(last_index, -1, -1):
-            if df.iloc[i]["timestamp"] != most_recent_timestamp:
-                # Slice from the next index to the end to get all rows with the most recent timestamp
-                return df.iloc[i + 1 :]
-        return df  # In case all rows have the same timestamp
-    else:
-        # return all the data
-        return df
 
 
 def update_table(_df):
@@ -171,36 +123,13 @@ def register_callbacks(app):
         Output("store-tec-data", "data"),
         [Input("interval-component", "n_intervals"), State("store-tec-data", "data")],
     )
-    def update_store_data(n, existing_store_data_json):
+    def update_store_data(n, existing_store_data):
         # Fetch data from the TEC interface
         df = tec_interface()._get_data()
+        return update_store(df, existing_store_data)
         
-            # check if the timestamp of the "new" data and most recent old data matches
-            # in that case, do not append the data as we already have it
-            # this happenes sometimes as TECInterface will only give new data every n second(s)
-        global _last_data_timestamp
         
-        new_timestamp = df.iloc[len(df) - 1]["timestamp"]
-        if _last_data_timestamp == new_timestamp:
-            return dash.no_update
         
-        _last_data_timestamp = new_timestamp
-
-        # if there is existing data, append to it. Otherwise, override it
-        if existing_store_data_json:
-            # convert existing data to df
-            existing_df = get_data_from_store(existing_store_data_json, most_recent=False)
-
-            # concat
-            updated_df = pd.concat([existing_df, df])
-
-            # convert back to json for storage
-            updated_store_data_json = df_to_base64(updated_df)
-        else:
-            # convert new data to json for storage
-            updated_store_data_json = df_to_base64(df)
-
-        return updated_store_data_json
 
     @app.callback(  # handles the table and graphs
         [
